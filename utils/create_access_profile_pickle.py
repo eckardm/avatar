eda98afc0672a4b9b5765da3d67993fa992b22d5
Max Eckard <eckardm@umich.edu>
2025-05-02 14:11:14 -0400

    Tidy paths and byte order mark

--- utils/create_access_profile_pickle.py
+++ utils/create_access_profile_pickle.py
@@ -4,13 +4,14 @@ import pickle
 
 access_profiles = {}
 
-with open(os.path.join('../', 'kaltura-access-export', 'kaltura-access-export-20210322.csv')) as f:
+with open(os.path.join('kaltura-access-export', 'kaltura-access-export-20210322.csv'), encoding='utf-8-sig') as f:
     reader = csv.DictReader(f)
     for row in reader:
+        print(row)
         mivideo_id = row['entry_id']
         # Based on our conversation, I think you will want to use the access control id field. Note that 876301 is reading room, 1694751 is public, and 2227181 is U-M campus.
         access_control_id = row['accessControlId']
         
         access_profiles[mivideo_id] = access_control_id
         
-pickle.dump(access_profiles, open(os.path.join('../', 'access_profiles.p'), 'wb'))
+pickle.dump(access_profiles, open(os.path.join('..', 'access_profiles.p'), 'wb'))
